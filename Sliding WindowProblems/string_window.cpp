0029157c37058d1f5a03fb986462a3a1cdc24446
ThibaMahlezana <thiba.ma@gmail.com>
2022-01-15 20:43:38 +0200

    added sliding window problems

---
+++ Sliding WindowProblems/string_window.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <climits>
+#include <string>
+
+using namespace std;
+
+string stringWindow(string s, string p){
+    int FP[256] = {0};
+    int FS[256] = {0};
+
+    for(int i = 0; i < p.length(); i++){
+        FP[p[i]]++;
+    }
+
+    int cnt = 0;
+    int start = 0;
+    int start_idx = -1;
+    int min_so_far = INT_MAX;
+    int window_size;
+
+    for(int i = 0; i < s.length(); i++){
+        char ch = s[i];
+        FS[s[i]]++;
+
+        if(FP[ch] != 0 and FS[ch] <= FP[ch]){
+            cnt += 1;
+        }
+
+        if(cnt == p.length()){
+            while(FP[s[start]] == 0 or FS[s[start]] > FP[s[start]]){
+                FS[s[start]]--;
+                start;
+            }
+
+            window_size = i - start + 1;
+            if(window_size < min_so_far){
+                min_so_far = window_size;
+                start_idx = start;
+            }
+        }
+    }
+    if(start_idx == -1){
+        return "No window found";
+    }
+    return s.substr(start_idx, min_so_far);
+}
+
+
+int main(){
+    string s, p;
+    cin >> s >> p;
+
+    cout << stringWindow(s, p) << endl;
+
+    return 0;
+}
