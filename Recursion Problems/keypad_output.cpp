0f4eeeec6894955ca54ff72788eb8946811e349a
ThibaMahlezana <thiba.ma@gmail.com>
2022-01-17 23:04:22 +0200

    added recursion problems

---
+++ Recursion Problems/keypad_output.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+string keypad[] = {"", "", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "XYZ"};
+
+void printKeypadOutput(string input, string output, int i = 0){
+    if(input[i] == '\0'){
+        cout << output << endl;
+        return;
+    }
+    int current_digit = input[i] - '0';
+    if(current_digit == 0 or current_digit == 1){
+        printKeypadOutput(input, output, i + 1);
+    }
+    for(int k = 0; k < keypad[current_digit].size(); k++){
+        printKeypadOutput(input, output + keypad[current_digit][k], i + 1);
+    }
+    return;
+}
+
+int main(){
+    string input;
+    cin >> input;
+    string output;
+    printKeypadOutput(input, output);
+
+    return 0;
+}
