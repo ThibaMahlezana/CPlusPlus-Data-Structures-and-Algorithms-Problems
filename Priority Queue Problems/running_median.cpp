d8a72198e0f11a070ae1cd07d4fb2b3f4b8926e2
ThibaMahlezana <thiba.ma@gmail.com>
2022-01-20 01:08:47 +0200

    added priority queue problems

---
+++ Priority Queue Problems/running_median.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    priority_queue<int> leftheap;
+    priority_queue<int, vector<int>, greater<int>> rightheap;
+
+    int d;
+    cin >> d;
+    leftheap.push(d);
+
+    float med = d;
+    cout << med << " ";
+
+    cin >> d;
+    while(d != -1){
+        if(leftheap.size() > rightheap.size()){
+            if(d < med){
+                rightheap.push(leftheap.top());
+                leftheap.pop();
+                leftheap.push(d);
+            }
+            else{
+                rightheap.push(d);
+            }
+            med = (leftheap.top() + rightheap()) / 2.0;
+        }
+        else if(leaftheap.size() == rightheap.size()){
+            if(d < med){
+                leftheap.push(d);
+                med = leftheap.top();
+            }
+            else{
+                rightheap.push(d);
+                med = rightheap.top();
+            }
+        }
+        else {
+            if(d < med){
+                leftheap.push(d);
+            }
+            else{
+                leftheap.push(rightheap.top());
+                rightheap.pop();
+                rightheap.push(d);
+            }
+            med = (leftheap.top() + rightheap.top()) / 2;
+        }
+        cout << med << " ";
+        cin >> d;
+    }
+    return 0;
+}
